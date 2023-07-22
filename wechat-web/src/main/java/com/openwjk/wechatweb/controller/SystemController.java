4d1343dad74c1bc9586a64a74e6a7882cba69f9e
junkai.wang <openwjk@gmail.com>
2023-07-22 12:13:46 +0800

    提交配置

--- wechat-web/src/main/java/com/openwjk/wechatweb/controller/SystemController.java
+++ wechat-web/src/main/java/com/openwjk/wechatweb/controller/SystemController.java
@@ -1,5 +1,6 @@
 package com.openwjk.wechatweb.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class SystemController {
     @GetMapping("/check.htm")
     public String checkRun() {
+        log.info("success.");
         return "success.";
     }
 }
